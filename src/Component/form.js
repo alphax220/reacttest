c7a417cd9db5554dafd949df8bd9326ce6d0b570
Alpha Diallo <DRAGON VERT@DESKTOP-DE9PHME>
2021-12-04 16:29:34 +0000

    Modif formulaire correction de validation

--- src/Component/form.js
+++ src/Component/form.js
@@ -3,23 +3,39 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 class Formulaire extends React.Component {
 
-   
-    state={
+   constructor(){
+    super()
+    this.state={
         user : "Open your account",
-        nameUser:"alpha"
+        nameUser:"alpha",
+        valuetext:null,
+        valuepass:null,
     }
 
 
+   }
+    
+
+
     handleSubmit= (event)=>{
+        
         const newValue = <h1>Connected</h1>
-        this.setState({user: newValue})
-        console.log(newValue)
+        const nullValue = <h1>Invalide</h1>
+        this.state.valuetext === "" || this.state.valuepass === "" || this.state.valuetext === null || this.state.valuepass === null? this.setState({user:nullValue}): this.setState({user:newValue})
+        console.log(this.state.valuetext)
+        console.log(this.state.valuepass)
     }
 
     handleChange = (event)=>{
         this.setState({user:event.target.value})
+        this.setState({valuetext:event.target.value})
         
     }
+
+    HandlePass = (event)=>
+    {
+        this.setState({valuepass:event.target.value})
+    }    
     render(){
          
         
@@ -31,10 +47,10 @@ class Formulaire extends React.Component {
                 <div>
                     <h2 > {this.state.user} </h2>
                 </div>
-                <input type="text" style={{marginBottom :20}} className="form-control" name="username" placeholder="Username" onChange={this.handleChange} />
+                <input type="text" style={{marginBottom :20}} className="form-control" name="username" placeholder="Username" onChange={this.handleChange} value={this.state.valuetext}/>
                 </div>
                 <div className="form-group">
-                <input type="password" className="form-control" name="password" placeholder="*****"/>
+                <input type="password" className="form-control" name="password" placeholder="*****" onChange={this.HandlePass} value={this.state.valuepass}/>
                 </div>
                 <div className="form-group">
                 <button  type="button" style={{marginTop :20}}  className="btn btn-primary btn-lg btn-block" onClick={this.handleSubmit} >Valider</button>
